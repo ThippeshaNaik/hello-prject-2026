18de9b5f26d6155eebb98d40cefd362ee355d589
root <root@ip-172-31-22-113.ec2.internal>
2026-01-07 02:35:55 +0000

    second commit

--- demo.py
+++ demo.py
@@ -1,7 +1,11 @@
-a = 1000
-b = 2000
-c = 3000
+#a = 1000
+#b = 2000
+#c = 3000
 
-print(a*b*c)
+#print(a*b*c)
 
-
+age = 21
+if age >= 21:
+    print("eligible for voting") 
+else:
+    print("not eligible for voting")
